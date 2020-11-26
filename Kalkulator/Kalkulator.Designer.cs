457f80a7972b23e741533ea692d56aac892a2f6a
Klegus <31622223+Klegus@users.noreply.github.com>
2020-11-26 22:21:25 +0100

    Add files via upload

---
+++ Kalkulator/Kalkulator.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace Kalkulator
+{
+    partial class Kalkulator
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN1 = new System.Windows.Forms.Button();
+            this.BTN2 = new System.Windows.Forms.Button();
+            this.BTN3 = new System.Windows.Forms.Button();
+            this.BTN4 = new System.Windows.Forms.Button();
+            this.BTN5 = new System.Windows.Forms.Button();
+            this.BTN6 = new System.Windows.Forms.Button();
+            this.BTN7 = new System.Windows.Forms.Button();
+            this.BTN8 = new System.Windows.Forms.Button();
+            this.BTN9 = new System.Windows.Forms.Button();
+            this.BTN0 = new System.Windows.Forms.Button();
+            this.BTNplus = new System.Windows.Forms.Button();
+            this.BTNminus = new System.Windows.Forms.Button();
+            this.BTNmulti = new System.Windows.Forms.Button();
+            this.BTNdiv = new System.Windows.Forms.Button();
+            this.LABEL = new System.Windows.Forms.TextBox();
+            this.BTNsum = new System.Windows.Forms.Button();
+            this.BTNreset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN1
+            // 
+            this.BTN1.Location = new System.Drawing.Point(12, 191);
+            this.BTN1.Name = "BTN1";
+            this.BTN1.Size = new System.Drawing.Size(46, 41);
+            this.BTN1.TabIndex = 0;
+            this.BTN1.Text = "1";
+            this.BTN1.UseVisualStyleBackColor = true;
+            this.BTN1.Click += new System.EventHandler(this.BTN1_Click);
+            // 
+            // BTN2
+            // 
+            this.BTN2.Location = new System.Drawing.Point(64, 191);
+            this.BTN2.Name = "BTN2";
+            this.BTN2.Size = new System.Drawing.Size(46, 41);
+            this.BTN2.TabIndex = 1;
+            this.BTN2.Text = "2";
+            this.BTN2.UseVisualStyleBackColor = true;
+            this.BTN2.Click += new System.EventHandler(this.BTN2_Click);
+            // 
+            // BTN3
+            // 
+            this.BTN3.Location = new System.Drawing.Point(116, 191);
+            this.BTN3.Name = "BTN3";
+            this.BTN3.Size = new System.Drawing.Size(46, 41);
+            this.BTN3.TabIndex = 2;
+            this.BTN3.Text = "3";
+            this.BTN3.UseVisualStyleBackColor = true;
+            this.BTN3.Click += new System.EventHandler(this.BTN3_Click);
+            // 
+            // BTN4
+            // 
+            this.BTN4.Location = new System.Drawing.Point(12, 144);
+            this.BTN4.Name = "BTN4";
+            this.BTN4.Size = new System.Drawing.Size(46, 41);
+            this.BTN4.TabIndex = 3;
+            this.BTN4.Text = "4";
+            this.BTN4.UseVisualStyleBackColor = true;
+            this.BTN4.Click += new System.EventHandler(this.BTN4_Click);
+            // 
+            // BTN5
+            // 
+            this.BTN5.Location = new System.Drawing.Point(64, 144);
+            this.BTN5.Name = "BTN5";
+            this.BTN5.Size = new System.Drawing.Size(46, 41);
+            this.BTN5.TabIndex = 4;
+            this.BTN5.Text = "5";
+            this.BTN5.UseVisualStyleBackColor = true;
+            this.BTN5.Click += new System.EventHandler(this.BTN5_Click);
+            // 
+            // BTN6
+            // 
+            this.BTN6.Location = new System.Drawing.Point(116, 144);
+            this.BTN6.Name = "BTN6";
+            this.BTN6.Size = new System.Drawing.Size(46, 41);
+            this.BTN6.TabIndex = 5;
+            this.BTN6.Text = "6";
+            this.BTN6.UseVisualStyleBackColor = true;
+            this.BTN6.Click += new System.EventHandler(this.BTN6_Click);
+            // 
+            // BTN7
+            // 
+            this.BTN7.Location = new System.Drawing.Point(12, 97);
+            this.BTN7.Name = "BTN7";
+            this.BTN7.Size = new System.Drawing.Size(46, 41);
+            this.BTN7.TabIndex = 6;
+            this.BTN7.Text = "7";
+            this.BTN7.UseVisualStyleBackColor = true;
+            this.BTN7.Click += new System.EventHandler(this.BTN7_Click);
+            // 
+            // BTN8
+            // 
+            this.BTN8.Location = new System.Drawing.Point(64, 97);
+            this.BTN8.Name = "BTN8";
+            this.BTN8.Size = new System.Drawing.Size(46, 41);
+            this.BTN8.TabIndex = 7;
+            this.BTN8.Text = "8";
+            this.BTN8.UseVisualStyleBackColor = true;
+            this.BTN8.Click += new System.EventHandler(this.BTN8_Click);
+            // 
+            // BTN9
+            // 
+            this.BTN9.Location = new System.Drawing.Point(116, 97);
+            this.BTN9.Name = "BTN9";
+            this.BTN9.Size = new System.Drawing.Size(46, 41);
+            this.BTN9.TabIndex = 8;
+            this.BTN9.Text = "9";
+            this.BTN9.UseVisualStyleBackColor = true;
+            this.BTN9.Click += new System.EventHandler(this.BTN9_Click);
+            // 
+            // BTN0
+            // 
+            this.BTN0.Location = new System.Drawing.Point(64, 238);
+            this.BTN0.Name = "BTN0";
+            this.BTN0.Size = new System.Drawing.Size(46, 41);
+            this.BTN0.TabIndex = 9;
+            this.BTN0.Text = "0";
+            this.BTN0.UseVisualStyleBackColor = true;
+            this.BTN0.Click += new System.EventHandler(this.BTN0_Click);
+            // 
+            // BTNplus
+            // 
+            this.BTNplus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTNplus.Location = new System.Drawing.Point(181, 97);
+            this.BTNplus.Name = "BTNplus";
+            this.BTNplus.Size = new System.Drawing.Size(46, 41);
+            this.BTNplus.TabIndex = 10;
+            this.BTNplus.Text = "+";
+            this.BTNplus.UseVisualStyleBackColor = true;
+            this.BTNplus.Click += new System.EventHandler(this.BTNplus_Click);
+            // 
+            // BTNminus
+            // 
+            this.BTNminus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTNminus.Location = new System.Drawing.Point(181, 144);
+            this.BTNminus.Name = "BTNminus";
+            this.BTNminus.Size = new System.Drawing.Size(46, 41);
+            this.BTNminus.TabIndex = 11;
+            this.BTNminus.Text = "-";
+            this.BTNminus.UseVisualStyleBackColor = true;
+            this.BTNminus.Click += new System.EventHandler(this.BTNminus_Click);
+            // 
+            // BTNmulti
+            // 
+            this.BTNmulti.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTNmulti.Location = new System.Drawing.Point(181, 191);
+            this.BTNmulti.Name = "BTNmulti";
+            this.BTNmulti.Size = new System.Drawing.Size(46, 41);
+            this.BTNmulti.TabIndex = 12;
+            this.BTNmulti.Text = "*";
+            this.BTNmulti.UseVisualStyleBackColor = true;
+            this.BTNmulti.Click += new System.EventHandler(this.BTNmulti_Click);
+            // 
+            // BTNdiv
+            // 
+            this.BTNdiv.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTNdiv.Location = new System.Drawing.Point(181, 236);
+            this.BTNdiv.Name = "BTNdiv";
+            this.BTNdiv.Size = new System.Drawing.Size(46, 41);
+            this.BTNdiv.TabIndex = 13;
+            this.BTNdiv.Text = "/";
+            this.BTNdiv.UseVisualStyleBackColor = true;
+            this.BTNdiv.Click += new System.EventHandler(this.BTNdiv_Click);
+            // 
+            // LABEL
+            // 
+            this.LABEL.Location = new System.Drawing.Point(12, 12);
+            this.LABEL.Multiline = true;
+            this.LABEL.Name = "LABEL";
+            this.LABEL.Size = new System.Drawing.Size(203, 79);
+            this.LABEL.TabIndex = 14;
+            this.LABEL.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LABEL_KeyPress);
+            // 
+            // BTNsum
+            // 
+            this.BTNsum.Location = new System.Drawing.Point(116, 238);
+            this.BTNsum.Name = "BTNsum";
+            this.BTNsum.Size = new System.Drawing.Size(46, 41);
+            this.BTNsum.TabIndex = 15;
+            this.BTNsum.Text = "=";
+            this.BTNsum.UseVisualStyleBackColor = true;
+            this.BTNsum.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // BTNreset
+            // 
+            this.BTNreset.Cursor = System.Windows.Forms.Cursors.Default;
+            this.BTNreset.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTNreset.Location = new System.Drawing.Point(12, 238);
+            this.BTNreset.Name = "BTNreset";
+            this.BTNreset.Size = new System.Drawing.Size(46, 41);
+            this.BTNreset.TabIndex = 16;
+            this.BTNreset.Text = "CLEAR";
+            this.BTNreset.UseVisualStyleBackColor = true;
+            this.BTNreset.Click += new System.EventHandler(this.BTNreset_Click);
+            // 
+            // Kalkulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 284);
+            this.Controls.Add(this.BTNreset);
+            this.Controls.Add(this.BTNsum);
+            this.Controls.Add(this.LABEL);
+            this.Controls.Add(this.BTNdiv);
+            this.Controls.Add(this.BTNmulti);
+            this.Controls.Add(this.BTNminus);
+            this.Controls.Add(this.BTNplus);
+            this.Controls.Add(this.BTN0);
+            this.Controls.Add(this.BTN9);
+            this.Controls.Add(this.BTN8);
+            this.Controls.Add(this.BTN7);
+            this.Controls.Add(this.BTN6);
+            this.Controls.Add(this.BTN5);
+            this.Controls.Add(this.BTN4);
+            this.Controls.Add(this.BTN3);
+            this.Controls.Add(this.BTN2);
+            this.Controls.Add(this.BTN1);
+            this.Name = "Kalkulator";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN1;
+        private System.Windows.Forms.Button BTN2;
+        private System.Windows.Forms.Button BTN3;
+        private System.Windows.Forms.Button BTN4;
+        private System.Windows.Forms.Button BTN5;
+        private System.Windows.Forms.Button BTN6;
+        private System.Windows.Forms.Button BTN7;
+        private System.Windows.Forms.Button BTN8;
+        private System.Windows.Forms.Button BTN9;
+        private System.Windows.Forms.Button BTN0;
+        private System.Windows.Forms.Button BTNplus;
+        private System.Windows.Forms.Button BTNminus;
+        private System.Windows.Forms.Button BTNmulti;
+        private System.Windows.Forms.Button BTNdiv;
+        private System.Windows.Forms.TextBox LABEL;
+        private System.Windows.Forms.Button BTNsum;
+        private System.Windows.Forms.Button BTNreset;
+    }
+}
+
---
+++ Kalkulator/Kalkulator.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Kalkulator : Form
+    {
+        double labelVAL1 = 0;
+        double labelVAL2 = 0;
+        double summ = 0;
+        string status = "";
+
+        public Kalkulator()
+        {
+            InitializeComponent();
+            LABEL.Text = "0";
+        }
+        void clearLABEL()
+        {
+            LABEL.Text = "";
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            
+            labelVAL2 = Convert.ToDouble(LABEL.Text);
+            if (status == "PLUS")
+            {
+                summ = labelVAL1 + labelVAL2;
+                clearLABEL();
+                LABEL.Text = summ.ToString();
+                status = "";
+                
+                labelVAL1 = 0;
+                labelVAL2 = 0;
+            }else if(status == "MINUS")
+            {
+                summ = labelVAL1 - labelVAL2;
+                clearLABEL();
+                LABEL.Text = summ.ToString();
+                status = "";
+
+                labelVAL1 = 0;
+                labelVAL2 = 0;
+            }
+            else if (status == "MULTI")
+            {
+                summ = labelVAL1 * labelVAL2;
+                clearLABEL();
+                LABEL.Text = summ.ToString();
+                status = "";
+
+                labelVAL1 = 0;
+                labelVAL2 = 0;
+            }
+            else if (status == "DIVIDE")
+            {
+                summ = labelVAL1 / labelVAL2;
+                clearLABEL();
+                LABEL.Text = summ.ToString();
+                status = "";
+
+                labelVAL1 = 0;
+                labelVAL2 = 0;
+            }
+
+        }
+
+        private void BTN1_Click(object sender, EventArgs e)
+        {
+            if(LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                summ = 0;
+                LABEL.Text = LABEL.Text + "1";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "1";
+            }
+            
+            
+
+        }
+
+        private void BTN2_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "2";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "2";
+            }
+        }
+
+        private void BTN3_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "3";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "3";
+            }
+        }
+
+        private void BTN4_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "4";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "4";
+            }
+        }
+
+        private void BTN5_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "5";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "5";
+            }
+        }
+
+        private void BTN6_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "6";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "6";
+            }
+        }
+
+        private void BTN7_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "7";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "7";
+            }
+        }
+
+        private void BTN8_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "8";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "8";
+            }
+        }
+
+        private void BTN9_Click(object sender, EventArgs e)
+        {
+            if (LABEL.Text == "0" || LABEL.Text == summ.ToString())
+            {
+                clearLABEL();
+                LABEL.Text = LABEL.Text + "9";
+            }
+            else
+            {
+                LABEL.Text = LABEL.Text + "9";
+            }
+        }
+
+        private void BTNplus_Click(object sender, EventArgs e)
+        {
+            labelVAL1 = Convert.ToDouble(LABEL.Text);
+            status = "PLUS";
+            clearLABEL();
+        }
+
+        private void BTNminus_Click(object sender, EventArgs e)
+        {
+            labelVAL1 = Convert.ToDouble(LABEL.Text);
+            status = "MINUS";
+            
+            clearLABEL();
+        }
+
+        private void BTNmulti_Click(object sender, EventArgs e)
+        {
+            labelVAL1 = Convert.ToDouble(LABEL.Text);
+            status = "MULTI";
+            
+            clearLABEL();
+        }
+        
+
+        private void BTNdiv_Click(object sender, EventArgs e)
+        {
+            labelVAL1 = Convert.ToDouble(LABEL.Text);
+            status = "DIVIDE";
+            clearLABEL();
+        }
+
+        private void BTNreset_Click(object sender, EventArgs e)
+        {
+            clearLABEL();
+        }
+
+        private void BTN0_Click(object sender, EventArgs e)
+        {
+            LABEL.Text = LABEL.Text + "0";
+        }
+
+        private void LABEL_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
